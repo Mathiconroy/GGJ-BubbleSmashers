3074577b6dae763fe458ce53b585447344438191
carlosvallory <carlvallory@gmail.com>
2025-01-25 20:49:22 -0300

    MENU UI

---
+++ Assets/ContinueButton.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ContinueButton : MonoBehaviour
+{
+    private int sceneToContinue;
+
+    public void ContinueGame ()
+    {
+        sceneToContinue = PlayerPrefs.GetInt("SavedScene");
+        if (sceneToContinue != 0)
+        { 
+            SceneManager.LoadScene(sceneToContinue);
+        } else
+        {
+            return;
+        }
+    }
+}
