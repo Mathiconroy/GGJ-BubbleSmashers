1b4f90c99ac0f893e7b13bf0ff966a1935e941ee
carlosvallory <carlvallory@gmail.com>
2025-01-26 08:49:29 -0300

    MENU SPRITES -  Highlighted, selected, pressed, disabled (2)

--- Assets/ContinueButton.cs
+++ Assets/ContinueButton.cs
@@ -4,6 +4,7 @@ using UnityEngine.SceneManagement;
 public class ContinueButton : MonoBehaviour
 {
     private int sceneToContinue;
+    public bool disabled = false;
 
     public void ContinueGame ()
     {
