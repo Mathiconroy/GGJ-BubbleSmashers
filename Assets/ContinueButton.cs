4b2e57a40c00f2f4186da25abdf3775ad608b49f
carlosvallory <carlvallory@gmail.com>
2025-01-26 10:26:45 -0300

    MENU - Escape

--- Assets/ContinueButton.cs
+++ Assets/ContinueButton.cs
@@ -6,6 +6,19 @@ public class ContinueButton : MonoBehaviour
     private int sceneToContinue;
     public bool disabled = false;
 
+    void Start()
+    {
+        // Comprueba si hay datos de guardado
+        if (PlayerPrefs.HasKey("SavedScene"))
+        {
+            disabled = false; // Habilita el botón si hay una partida guardada.
+        }
+        else
+        {
+            disabled = true; // Deshabilita el botón si no hay datos de guardado.
+        }
+    }
+
     public void ContinueGame ()
     {
         sceneToContinue = PlayerPrefs.GetInt("SavedScene");
