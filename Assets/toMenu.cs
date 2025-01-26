e3825f1662ad3e469b4ffb4e1d658cc2d5422bc7
carlosvallory <carlvallory@gmail.com>
2025-01-26 11:39:07 -0300

    MENU - Fuente

--- Assets/toMenu.cs
+++ Assets/toMenu.cs
@@ -1,10 +1,29 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class toMenu : MonoBehaviour
 {
+    GameController gameController;
+    bool disabled = false;
+    private void Start()
+    {
+        gameController = FindAnyObjectByType<GameController>();
+        disabled = Convert.ToBoolean(gameController.currentGameState);
+    }
+
     void Update()
     {
+        disabled = Convert.ToBoolean(gameController.currentGameState);
+
+        if (!disabled)
+        {
+            this.gameObject.SetActive(false);
+        } else
+        {
+            this.gameObject.SetActive(true);
+        }
+
         if (Input.GetKeyUp(KeyCode.Escape) || Input.GetKeyUp(KeyCode.Return))
         {
             LoadMainMenu();
